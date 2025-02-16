e7a1864956a39c958e2be940d69c69b340bb28eb
Mohd Zaid <zaid.ahmed.19nov@gmail.com>
2025-02-16 13:16:14 +0530

    changes done

--- Frontend/src/components/Quote.tsx
+++ Frontend/src/components/Quote.tsx
@@ -4,14 +4,14 @@ export const Quote = () => {
         <div className="flex justify-center">
             <div className="max-w-lg">
                 <div className="text-3xl font-bold">
-                    "The customer support I received was exceptional. The support team went above and beyond to address my concerns"
+                    Everyone has a story to tell. A place to read, write, and deepen your understanding.
                 </div>
                 <div className="max-w-md text-xl font-semibold text-left mt-4">
-                    Julies Winfield
+                Ultimately, our goal is to deepen our collective understanding of the world through the power of writing.
                 </div>
-                <div className="max-w-md text-sm font-light text-slate-400">
+                {/* <div className="max-w-md text-sm font-light text-slate-400">
                     CEO | Acme corp
-                </div>
+                </div> */}
             </div>
         </div>
         
