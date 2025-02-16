bb5d0fc4044dcb570a2ab275c069eb288ed2d89c
Mohd Zaid <zaid.ahmed.19nov@gmail.com>
2025-02-16 13:12:39 +0530

    minor changes done

--- Frontend/src/components/Auth.tsx
+++ Frontend/src/components/Auth.tsx
@@ -39,13 +39,13 @@ export const Auth = ({ type }: { type: "signup" | "signin" }) => {
                     </div>
                 </div>
                 <div className="pt-8">
-                    {type === "signup" ? <LabelledInput label="Name" placeholder="Harkirat Singh..." onChange={(e) => {
+                    {type === "signup" ? <LabelledInput label="Name" placeholder="Zaid Saifi..." onChange={(e) => {
                         setPostInputs({
                             ...postInputs,
                             name: e.target.value
                         })
                     }} /> : null}
-                    <LabelledInput label="Username" placeholder="harkirat@gmail.com" onChange={(e) => {
+                    <LabelledInput label="Username" placeholder="zaid@gmail.com" onChange={(e) => {
                         setPostInputs({
                             ...postInputs,
                             username: e.target.value
